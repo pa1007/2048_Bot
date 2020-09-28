3634244e4883ee035e5515ff9d60a296f8a72bbb
pa1007 <pa.fourriere@pa1007.fr>
2020-09-28 17:27:58 +0200

    Create game object

---
+++ src/main/java/dev/pa1007/java2048bot/Game.java
@@ -0,0 +1,43 @@
+package dev.pa1007.java2048bot;
+
+import java.util.List;
+
+public class Game {
+
+    private List<Case> casesSortedAndTyped;
+
+
+    public Game(List<Case> casesSortedAndTyped) {
+        this.casesSortedAndTyped = casesSortedAndTyped;
+    }
+
+    private String format(int val) {
+        if (val > 1000) {
+            return String.valueOf(val);
+        }
+        else if (val > 100) {
+            return " " + val;
+        }
+        else if (val > 10) {
+            return " " + val + " ";
+        }
+        else {
+            return " 0" + val + " ";
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int           i             = 0;
+        for (Case aCase : casesSortedAndTyped) {
+            stringBuilder.append("| ").append(format(aCase.getType().getVal())).append(" ");
+            i++;
+            if (i == 4) {
+                stringBuilder.append("|\n");
+                i = 0;
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
